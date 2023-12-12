a80e75401024b77b039c4c5fd7efca0982b759e9
ymtowya <kaigendo@126.com>
2023-12-11 23:19:40 -0800

    update shell train

--- train.sh
+++ train.sh
@@ -4,8 +4,6 @@ git clone https://github.com/ultralytics/yolov5
 pip install -q roboflow
 pip install roboflow
 pip install -qr requirements.txt
-cp phase1.py ./yolov5
-cd yolov5
 
 python ./train1.py
 
